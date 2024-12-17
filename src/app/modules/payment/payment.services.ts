6c3aaed4018d4d767265d0b8892b2b9dc07ff814
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-12-17 23:12:16 +0600

    Amar pay rediection

--- src/app/modules/payment/payment.services.ts
+++ src/app/modules/payment/payment.services.ts
@@ -1,22 +1,22 @@
-import httpStatus from "http-status";
-import path from "path";
-import http_error from "../../errors/http_error";
-import { promises as fs } from "fs";
-import axios from "axios";
-import { local_config } from "../../config";
-import prisma from "../../utils/prisma";
-import { PaymentStatus } from "@prisma/client";
+import httpStatus from 'http-status';
+import path from 'path';
+import http_error from '../../errors/http_error';
+import { promises as fs } from 'fs';
+import axios from 'axios';
+import { local_config } from '../../config';
+import prisma from '../../utils/prisma';
+import { PaymentStatus } from '@prisma/client';
 
 const success = async (trans_id: string) => {
-  const filePath = path.join(__dirname, "../../views/success.html");
+  const filePath = path.join(__dirname, '../../views/success.html');
 
   try {
-    const data = await fs.readFile(filePath, "utf8");
+    const data = await fs.readFile(filePath, 'utf8');
     const checkPayment = await axios.get(
       `https://sandbox.aamarpay.com/api/v1/trxcheck/request.php?request_id=${trans_id}&store_id=${local_config.store_id}&signature_key=${local_config.signature_key}&type=json`
     );
 
-    if (checkPayment?.data?.pay_status === "Successful") {
+    if (checkPayment?.data?.pay_status === 'Successful') {
       await prisma.order.updateMany({
         where: { transaction_id: trans_id },
         data: { payment_status: PaymentStatus.COMPLETED },
@@ -25,21 +25,22 @@ const success = async (trans_id: string) => {
 
     return data;
   } catch (error) {
-    throw new http_error(Number(httpStatus[500]), "Error reading file");
+    throw new http_error(Number(httpStatus[500]), 'Error reading file');
   }
 };
 
+//redirect after payment failed
 const failed = async (trans_id: string) => {
-  const filePath = path.join(__dirname, "../../views/failed.html");
+  const filePath = path.join(__dirname, '../../views/failed.html');
   try {
-    const data = await fs.readFile(filePath, "utf8");
+    const data = await fs.readFile(filePath, 'utf8');
     await prisma.order.updateMany({
       where: { transaction_id: trans_id },
       data: { payment_status: PaymentStatus.FAILED },
     });
     return data;
   } catch (error) {
-    throw new http_error(Number(httpStatus[500]), "Error reading file");
+    throw new http_error(Number(httpStatus[500]), 'Error reading file');
   }
 };
 
