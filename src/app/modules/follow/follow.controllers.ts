a8bceba75e82379a154bd9fd753b164df8183229
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-12-17 23:16:08 +0600

    User image load complete

--- src/app/modules/follow/follow.controllers.ts
+++ src/app/modules/follow/follow.controllers.ts
@@ -1,14 +1,14 @@
-import httpStatus from "http-status";
-import catch_async from "../../utils/catch_async";
-import send_response from "../../utils/send_response";
-import { follow_services } from "./follow.services";
+import httpStatus from 'http-status';
+import catch_async from '../../utils/catch_async';
+import send_response from '../../utils/send_response';
+import { follow_services } from './follow.services';
 
 const follow_shop = catch_async(async (req, res) => {
   const result = await follow_services.follow_shop(req.body, req.user);
 
   send_response(res, {
     status: httpStatus.OK,
-    message: "User registered successfully.",
+    message: 'User register successfully.',
     data: result,
   });
 });
